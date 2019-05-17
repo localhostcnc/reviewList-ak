a5c2a57643227c6cc87685453798c9717997dc00
EtiWitty <atiyeh.keshavarz@gmail.com>
2019-05-16 23:55:09 -0700

    Finalized working schema and seed for sql

--- server/schema.sql
+++ server/schema.sql
@@ -29,22 +29,22 @@ INSERT INTO review_attributes (name) VALUES ("Value");
 /* ~~~~~~~~~~~~~~~~~~~~~ reviews ~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 CREATE TABLE reviews (
-    reviews_id INT AUTO_INCREMENT UNIQUE,
+    review_id INT AUTO_INCREMENT UNIQUE,
     property_id INT,
-    date DATETIME,
+    date TIMESTAMP,
     author_id INT,
     content TEXT NOT NULL,
     FOREIGN KEY (author_id) REFERENCES review_author(author_id),
-    PRIMARY KEY (reviews_id)
+    PRIMARY KEY (review_id)
 );
 
 /* ~~~~~~~~~~~~~~~~~~~~~ reviews ~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 CREATE TABLE ratings (
     rating_id INT AUTO_INCREMENT UNIQUE,
-    reviews_id INT,
+    review_id INT,
     attribute_id INT,
-    FOREIGN KEY (reviews_id) REFERENCES reviews(reviews_id),
+    FOREIGN KEY (review_id) REFERENCES reviews(review_id),
     FOREIGN KEY (attribute_id) REFERENCES review_attributes(attribute_id),
     value INT
 );
