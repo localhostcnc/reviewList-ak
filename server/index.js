bfe03476aec23e8187e6f5a8a8dc9fc1a46dfbf4
EtiWitty <atiyeh.keshavarz@gmail.com>
2019-05-16 19:30:26 -0700

    initiated the server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const db = require('./db/controller.js');
+const bodyPraser = require('bode-parser');
+const PORT = 3030;
+
+app.use(express.static('client/dist/'));
+app.use(bodyPraser.json());
+
+app.get('/reviews/summary/reviewcount', (req, res) => {
+    res.send('Welcome to the server hacker!');
+})
+
+app.listen(PORT, () => {
+    console.log('server is listening on port: ', PORT);
+});
