3aeed0569555738577e8a3259c4070195785f52d
EtiWitty <atiyeh.keshavarz@gmail.com>
2019-05-17 15:07:07 -0700

    working on connecting db to server

--- server/db/controller.js
+++ server/db/controller.js
@@ -1,3 +1,18 @@
 
 const connection = require('./index.js');
+const getAll = function(res){
+    // execute my query to the db
+    connection.query('SELECT * FROM reviews', (err,data) => {
+        if (err) {
+            res.status(500);
+            res.send(err);
+        } else {
+            res.status(200);
+            res.send(data);
+        }
+    });
+}
 
+module.exports = {
+    getAll: getAll
+}
