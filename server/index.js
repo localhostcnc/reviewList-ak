c7c38fada481af63e28fd967c2194961fb1717ba
EtiWitty <atiyeh.keshavarz@gmail.com>
2019-05-21 17:57:09 -0700

    finished all queries and APIs

--- server/index.js
+++ server/index.js
@@ -1,20 +1,32 @@
+/* eslint-disable func-names */
 const express = require('express');
 
 const app = express();
 const bodyPraser = require('body-parser');
 const db = require('./db/controller.js');
 
-const PORT = 3030;
+const port = process.env.PORT || 3030;
 
 app.use(express.static('client/dist/'));
 app.use(bodyPraser.json());
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// let resultHandler = function (err, data) {
+//   if (err) {
+//     res.status(500);
+//     res.send(err);
+//   } else {
+//     res.status(200);
+//     res.send(data);
+//   }
+// };
 
 app.get('/client', (req, res) => {
   res.send('Welcome to the client hacker!');
 });
 
 app.get('/reviews', (req, res) => {
-  db.getAll((err, data) => {
+  db.getReviewItems((err, data) => {
     if (err) {
       res.status(500);
       res.send(err);
@@ -25,6 +37,37 @@ app.get('/reviews', (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log('server is listening on port: ', PORT);
+
+app.get('/attributesrating/:attribute?', (req, res) => {
+  db.getRatingAttributes((err, data) => {
+    if (err) {
+      res.status(500);
+      res.send(err);
+    } else {
+      res.status(200);
+      let result;
+      if (req.params.attribute) {
+        result = data.filter(obj => obj.attribute_name.toLowerCase() === req.params.attribute);
+      } else {
+        result = data;
+      }
+      res.send(result);
+    }
+  });
+});
+
+app.get('/reviews/summary/reviewcount', (req, res) => {
+  db.getReviewsCount((err, data) => {
+    if (err) {
+      res.status(500);
+      res.send(err);
+    } else {
+      res.status(200);
+      res.send(data);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}...`);
 });
--- test/test.js
+++ test/test.js
@@ -1,19 +1,8 @@
 
 const supertest = require('supertest');
 const should = require('should');
+const assert = require('assert');
 
 const server = supertest.agent('http://localhost:3030');
 
-describe('SIMPLE unit test', () => {
-  it('should return homepage', (done) => {
-    server
-      .get('/')
-      .expect('content-type', /json/)
-      .expect(200)
-      .end((err, res) => {
-        res.status.should.equal(200);
-        // res.body.error.should.equal(false);
-        done();
-      });
-  });
-});
+
