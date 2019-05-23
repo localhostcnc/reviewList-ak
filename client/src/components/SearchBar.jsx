1fbc5ef2bec06e7e89aa20422938fc168724f610
EtiWitty <atiyeh.keshavarz@gmail.com>
2019-05-23 16:15:35 -0700

    Revert "change eslint rules to airbnb"
    This reverts commit d11c0be42644a31f782930065b4c6d3c5bfc567b.

---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super();
+    this.state = null;
+  }
+
+  // filter by any word in any review item (content)
+
+  render() {
+    return (
+      <div>I am search Bar</div>
+    );
+  }
+}
+
+export default SearchBar;
